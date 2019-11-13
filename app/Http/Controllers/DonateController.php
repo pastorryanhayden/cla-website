5a4c7d0ce5836e6a0bebf1345024701ba65b6d5b
Ryan Hayden <pastorryanhayden@gmail.com>
2019-11-13 11:18:33 -0600

    fixed stripe key

--- app/Http/Controllers/DonateController.php
+++ app/Http/Controllers/DonateController.php
@@ -51,7 +51,7 @@ class DonateController extends Controller
 
     protected function doPayment($token, $email, $amount)
     {
-        Stripe::setApiKey(config('services.stripe.secret'));
+        Stripe::setApiKey(env('STRIPE_KEY', null););
         $name = env('APP_NAME');
         $connect = env('STRIPE_CONNECT', null);
         $customer = Customer::create(array(
